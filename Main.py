556a43e612c917246af0ab0f6c45a60f2ea9f7d3
E7lektronX <furkanbuberx@gmail.com>
2024-07-15 20:39:12 +0300

    Remastered Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,9 @@ import os
 file_path = "AFETZEDE.xlsx"
 df = pd.read_excel(file_path)
 
+output_all = r"C:\Users\furka\Desktop\E7X\Code\Python\Veri Analizi\All"
+os.makedirs(output_all, exist_ok=True)
+
 # İl ve ilçe bilgileri
 iller = ['HATAY', 'KAHRAMANMARAŞ', 'MALATYA', 'GAZİANTEP', 'OSMANİYE', 'ADIYAMAN', 'DİYARBAKIR', 'ADANA']
 ilceler = {
@@ -32,7 +35,7 @@ output_paths = {
     'ADIYAMAN': r'C:\Users\furka\Desktop\E7X\Code\Python\Veri Analizi\Adiyaman'
 }
 
-# Kaydedilicek Klasör Kontrol
+# Klasörleri oluştur
 for path in output_paths.values():
     os.makedirs(path, exist_ok=True)
 
@@ -41,9 +44,46 @@ for il in iller:
     il_df = df[df['GELDİĞİ İL'] == il]
     il_df.to_excel(os.path.join(output_paths[il], f"{il}.xlsx"), index=False)
 
+    # İl bazında toplam sayım
+    il_count = len(il_df)
+    il_all = pd.DataFrame({"İL": [il], "SAYI": [il_count]})
+
+    all_file = os.path.join(output_all, "Toplam.xlsx")
+    if os.path.exists(all_file):
+        all_df = pd.read_excel(all_file)
+        all_df = pd.concat([all_df, il_all], ignore_index=True)
+    else:
+        all_df = il_all
+
+    all_df.to_excel(all_file, index=False)
+
+    # İlçe bazında veri sayımı
+    rows = []
+    rows.append([il, "TOPLAM", il_count])
+
     if il in ilceler:
         for ilce in ilceler[il]:
             ilce_df = il_df[il_df['GELDİĞİ İLÇE'] == ilce]
+            ilce_count = len(ilce_df)
+            rows.append([il, ilce, ilce_count])
+
             ilce_df.to_excel(os.path.join(output_paths[il], f"{ilce}.xlsx"), index=False)
 
-print("Tüm işlemler başarılı!")
+    count_df = pd.DataFrame(rows, columns=["İL", "İLÇE", "SAYI"])
+
+    # İlçeler bazında toplam sayımı ekleyerek dosyaları kaydet
+    with pd.ExcelWriter(os.path.join(output_paths[il], f"{il}.xlsx"), engine='openpyxl', mode='a') as writer:
+        count_df.to_excel(writer, sheet_name="İlçe Sayım", index=False)
+
+
+print("Hatay Başarılı")
+print("Kahramanmaraş Başarılı")
+print("Malatya Başarılı")
+print("GaziAntep Başarılı")
+print("Osmaniye Başarılı")
+print("Adıyaman Başarılı")
+print("DiyarBakır Başarılı")
+print("Adana Başarılı")
+print("By Ramazan Furkan Büber")
+print("Copyright (c) 2024 Ramazan Furkan Büber")
+
